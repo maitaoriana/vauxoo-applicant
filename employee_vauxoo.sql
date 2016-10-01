b65e8e7ae4decc4b49a16a16eabeb75b28641ce4
maitaoriana <maitaoriana@gmail.com>
2016-10-01 10:53:30 -0400

    Add files via upload
    sql2

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -50,7 +50,57 @@ INSERT INTO employee(first_name, last_name, id_department)
 INSERT INTO employee(first_name, last_name, id_department)
     VALUES ('Alejandro','Maita',4);
 
---CREATE TABLE employee_hobby (
---);
+CREATE TABLE employee_hobby (
+  id serial NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  description VARCHAR(200),
+  CONSTRAINT employee_hobby_pkey PRIMARY KEY (id)
+);
 
--- ...
+CREATE TABLE employee_has_hobby
+(
+  id_hobby serial NOT NULL,
+  id_employee serial NOT NULL,
+  CONSTRAINT employee_has_hobby_pkey PRIMARY KEY (id_hobby, id_employee),
+  CONSTRAINT employee_has_hobby_id_employee_fkey FOREIGN KEY (id_employee)
+      REFERENCES employee (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT employee_has_hobby_id_hobby_fkey FOREIGN KEY (id_hobby)
+      REFERENCES employee_hobby (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+INSERT INTO employee_hobby(name, description)
+    VALUES ('Jugar Futbol','Jugar');
+
+INSERT INTO employee_hobby(name)
+    VALUES ('Cantar');
+
+INSERT INTO employee_hobby(name)
+    VALUES ('Pintar');
+
+INSERT INTO employee_has_hobby(id_hobby, id_employee)
+    VALUES (1, 3);
+
+INSERT INTO employee_has_hobby(id_hobby, id_employee)
+    VALUES (1, 4);
+
+INSERT INTO employee_has_hobby(id_hobby, id_employee)
+    VALUES (2, 4);
+
+INSERT INTO employee_has_hobby(id_hobby, id_employee)
+    VALUES (3, 3);
+
+INSERT INTO employee_has_hobby(id_hobby, id_employee)
+    VALUES (2, 1);
+
+INSERT INTO employee_has_hobby(id_hobby, id_employee)
+    VALUES (3, 2);
+
+INSERT INTO employee_has_hobby(id_hobby, id_employee)
+    VALUES (3, 1);
+
+INSERT INTO employee_has_hobby(id_hobby, id_employee)
+    VALUES (2, 2);
+
+
