ace00125cb75cd38cf97dfc50138fd1b399fd97c
maitaoriana <maitaoriana@gmail.com>
2016-10-01 22:56:14 -0400

    py1

--- primes.py
+++ primes.py
@@ -1,16 +1,34 @@
-"""
-Your module documentation here
+""" Modulo para determinar numeros primos
+
+Desarrollado por: Oriana Maita
+
 """
 
 
 class PrimeClass(object):
-    """
-    Your class documentation here
+    """Clase de Numeros Primos
+
+    Esta clase da soporte para los numeros primos
+
     """
 
     def is_prime(self, num_int):
+        """Determina si un numero es primo
+
+        Dado un numero entero, retorna verdadero en caso de
+        ser primo y falso en el caso contrario.
+
         """
-        Your method documentation here
-        """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+
+        if num_int < 2:
+            es_primo = False
+        else:
+            es_primo = True
+
+        divisor = 2
+        while es_primo and divisor < num_int:
+            if num_int % divisor == 0:
+                es_primo = False
+            divisor = divisor + 1
+
+        return es_primo
